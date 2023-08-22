399e2f1484e34e7c46656a92e600bcf25c9a2b55
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-22 16:56:09 +0000

    Add component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getGreetings } from "../redux/greet/greetingsSlice";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greetings.greeting);
+  console.log(greeting)
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h3>{`${greeting}, Welcome to my app`}</h3>
+    </div>
+  );
+}
+
+export default Greeting;
